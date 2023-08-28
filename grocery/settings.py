c0ccfbcb947671759396ab2578c4d8fcb3e37946
manish021124 <maneeshbalami@gmail.com>
2023-08-28 14:03:20 +0545

    update setting

--- grocery/settings.py
+++ grocery/settings.py
@@ -29,9 +29,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['djangogrocerylist-production.up.railway.app', '127.0.0.1']
 
-
+CSRF_TRUSTED_ORIGINS = ['https://djangogrocerylist-production.up.railway.app/']
 # Application definition
 
 INSTALLED_APPS = [
