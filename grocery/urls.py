48e38523255506fddf24aacc53bcc6b844b27769
manish021124 <maneeshbalami@gmail.com>
2023-08-29 17:09:18 +0545

    update templates

--- grocery/urls.py
+++ grocery/urls.py
@@ -20,9 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('list.urls')),
-    path('', include('user.urls')),
+    path('user/', include('user.urls')),
     path('admin/', admin.site.urls),
 ]
-
-
-
