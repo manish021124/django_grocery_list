659693918e8e31fcac3d8e538ccb7b909e004802
manish021124 <maneeshbalami@gmail.com>
2023-08-29 13:34:55 +0545

    update key

--- grocery/settings.py
+++ grocery/settings.py
@@ -15,16 +15,17 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 import os
 
-load_dotenv()
+# load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+# SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = 'django-insecure-q@l$ifee9^0gs1yh&z4o^a9isj5dp5yo7cf$4u037guvj@n+&*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
