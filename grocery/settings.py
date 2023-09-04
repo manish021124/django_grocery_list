320c75566bb05275a9ef3f3a38b218e1d514a558
manish021124 <maneeshbalami@gmail.com>
2023-09-04 11:21:27 +0545

    upadte

--- grocery/settings.py
+++ grocery/settings.py
@@ -24,7 +24,7 @@ load_dotenv()
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = 'django-insecure-q@l$ifee9^0gs1yh&z4o^a9isj5dp5yo7cf$4u037guvj@n+&*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
