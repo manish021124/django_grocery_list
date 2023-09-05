008a3e22317cd9277abe71269370fdbf911d07fb
manish021124 <maneeshbalami@gmail.com>
2023-09-05 11:24:22 +0545

    update

--- user/views.py
+++ user/views.py
@@ -22,7 +22,7 @@ def signup(request):
             if form.is_valid():
                 form.save()
                 messages.success(request, "Congratulation! You have created an account successfully. Pleae log in to continue.")
-                return redirect('user/login')
+                return redirect('login')
                 
         context = {'form': form}
         return render(request, 'signup.html', context)
