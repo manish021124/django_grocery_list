f98790b278554f468a8c59d8d79c0c756b81a100
manish021124 <maneeshbalami@gmail.com>
2023-09-05 11:53:01 +0545

    update

--- grocery/settings.py
+++ grocery/settings.py
@@ -27,7 +27,7 @@ load_dotenv()
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ['DEBUG']
+DEBUG = os.getenv['DEBUG']
 
 ALLOWED_HOSTS = ['djangogrocerylist-production.up.railway.app', '127.0.0.1']
 
