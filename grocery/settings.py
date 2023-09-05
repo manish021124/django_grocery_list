da091b1f96d7351a719bbbdd56830a7f10da803a
manish021124 <maneeshbalami@gmail.com>
2023-09-05 11:27:12 +0545

    update hosts

--- grocery/settings.py
+++ grocery/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['djangogrocerylist-production.up.railway.app', '127.0.0.1']
+ALLOWED_HOSTS = ['djangogrocerylist-production.up.railway.app/', '127.0.0.1']
 
 CSRF_TRUSTED_ORIGINS = ['https://djangogrocerylist-production.up.railway.app', 'https://www.djangogrocerylist-production.up.railway.app']
 # Application definition
