bc0d9c93e9de5d891a9b210e94dc89e755f95815
manish021124 <maneeshbalami@gmail.com>
2023-09-10 18:19:11 +0545

    update

--- user/urls.py
+++ user/urls.py
@@ -1,8 +1,6 @@
 from . import views
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -13,7 +11,3 @@ urlpatterns = [
     path('deleteUser/<str:username>/', views.deleteUser, name='deleteUser'),
     path('change_password/', views.change_password, name='change_password')
 ]
-
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
