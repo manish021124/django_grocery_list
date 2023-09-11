8bab6d7a46f25d1dee44aaed56cb6a02fcb36731
manish021124 <maneeshbalami@gmail.com>
2023-09-11 17:54:59 +0545

    update

--- grocery/urls.py
+++ grocery/urls.py
@@ -24,5 +24,4 @@ urlpatterns = [
     path('', include('list.urls')),
     path('user/', include('user.urls')),
     path('admin/', admin.site.urls),
-    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
