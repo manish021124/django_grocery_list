1be5db87a48603eee0ab07b7ffd335d085388929
manish <maneeshbalami@gmail.com>
2023-09-26 11:00:07 +0545

    update setting

--- grocery/settings.py
+++ grocery/settings.py
@@ -15,13 +15,13 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
 import dj_database_url
 
 load_dotenv()
 
-DATABASE_URL = 'postgresql://postgres:vm3kHHh7gWYNh6nR02Iu@containers-us-west-36.railway.app:5720/railway'
+DATABASE_URL = os.getenv('DATABASE_URL')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
